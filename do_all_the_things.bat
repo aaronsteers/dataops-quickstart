967292405242146169c28bf7412682dfe7bc5d53
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2019-07-05 15:49:32 -0700

    delete older commented-out btsadmin code

--- do_all_the_things.bat
+++ do_all_the_things.bat
@@ -5,79 +5,46 @@ echo Installing Chocolatey - "The package manager for Windows"...
 echo Chocolatey install complete.
 echo:
 pause
-
-echo Enabling Hyper-V virtualization (required by Docker) and WSL Linux support...
+echo Enabling Hyper-V virtualization (required by Docker) and WSL (Linux Subsystem)...
 DISM /online /enable-feature /featurename:Microsoft-Hyper-V-All
+echo:
+echo:
 DISM /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux
-
+echo:
+echo Installation of Hyper-V and WSL complete!
+echo:
+echo Installing Docker Desktop ('choco install docker-desktop')...
+echo:
 choco install docker-desktop
+echo:
+echo Installing Git ('choco install git.install --params "/GitOnlyOnPath /SChannel /NoAutoCrlf /WindowsTerminal"')...
+echo:
 choco install git.install --params "/GitOnlyOnPath /SChannel /NoAutoCrlf /WindowsTerminal"
+echo:
+echo Installing VS Code ('choco install vscode')...
+echo:
 choco install vscode
+echo:
+echo Installing Ubuntu 18.04 ('choco install wsl-ubuntu-1804')...
+echo:
 choco install wsl-ubuntu-1804
-
-REM Launch Ubuntu:
+echo:
+echo All installations complete!
+echo:
 echo The next step is to launch the Ubuntu system.
-echo Ubuntu will now launch and the Linux kernel will finalize its updates.
+echo Ubuntu will now open a new terminal in order to complete the setup.
 echo Type 'exit' once complete to return to this installer....
+echo:
 bash exit
-
 echo:
 echo Testing that Docker is working...
 echo:
 docker run -it hello-world
-
+echo:
+echo Docker test complete!
+echo:
 echo Setup script execution complete!
 echo:
 echo:
 pause
 echo on
-
-REM Deprecated bitsadmin in favor of choco install
-REM REM Download and install the latest stable version of Docker Desktop:
-REM echo Attempting to download Docker for Windows...
-REM echo NOTE: If the default download link does not work, you can create an account with DockerHub.com and
-REM echo       download manually from: https://docs.docker.com/docker-for-windows/install/
-REM cls
-REM echo Downloading Docker...
-REM bitsadmin /transfer "Docker Download" /download /priority normal https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe c:\Files\Docker%20for%20Windows%20Installer.exe
-REM echo Attempting to install Docker for Windows...
-REM c:\Files\Docker%20for%20Windows%20Installer.exe
-
-REM echo Downloading Git...
-REM echo WARNING: The version number is built-in to the URL: Downloading version '2.21.0-64-bit'...
-REM cls
-REM echo Downloading Git...
-REM set GIT_VERSION=2.22.0
-REM set GIT_DOWNLOAD_PATH=https://github.com/git-for-windows/git/releases/download/v%GIT_VERSION%.windows.1/Git-%GIT_VERSION%-64-bit.exe
-REM bitsadmin /transfer "Git Download" /download /priority normal %GIT_DOWNLOAD_PATH% c:\Files\Git-%GIT_VERSION%-64-bit.exe
-REM echo Installing Git from '%GIT_DOWNLOAD_PATH%'...
-REM C:\Files\Git-%GIT_VERSION%-64-bit.exe /LOADINF="git-install-config.ini" /SAVEINF=".logs/git-install-config.new.ini" /VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"
-
-
-REM echo Creating a new folder 'C:\Files\' for downloads...
-REM mkdir C:\Files
-REM mkdir .logs
-
-REM echo Downloading the latest version of VS Code...
-REM cls
-REM echo Downloading VS Code...
-REM bitsadmin /rawreturn /transfer "VS Code Download" /download /priority normal https://aka.ms/win32-x64-user-stable c:\Files\VS-Code-Install-latest.exe
-REM echo Installing VS Code...
-REM runas /trustlevel:0x20000 c:\Files\VS-Code-Install-latest.exe
-REM echo The VS Code installer should now have opened in a new window. Complete the installation and then return to this window.
-REM echo:
-REM pause
-
-REM REM Install the latest stable version of Ubuntu from Windows Store:
-REM echo Loading the Ubuntu app in the Microsoft Store...
-REM explorer "ms-windows-store://pdp/?ProductId=9NBLGGH4MSV6"
-REM echo Windows store should now have opened in a new window. Click 'Install' to install the Ubuntu app and then return to this window.
-REM echo:
-REM echo Note: If you have not used the Microsoft Store previously, you may need to log in and/or 
-REM echo       create a Microsoft account if you do not have one already.
-REM echo:
-REM echo Note: Once the install completes, you will need to create a new Linux username and password. The
-REM echo       username cannot contain special characters (including '.'). The password is also not synced
-REM echo       with or rotated on the same schedule as your Windows credentials.
-REM echo:
-REM pause
